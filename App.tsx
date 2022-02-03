3fc9078cc4c3fe5c20f3ec8d2aac6a4058fd4b00
zenatrick <zenatrick@gmail.com>
2022-02-04 03:24:21 +0800

    Add home screen and basic navigation

--- App.tsx
+++ App.tsx
@@ -1,14 +1,62 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  FlatList,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import {
+  createNativeStackNavigator,
+  NativeStackScreenProps,
+} from '@react-navigation/native-stack';
+import * as pages from './pages';
+import type { PageName } from './pages';
 
-export default function App() {
+type RootStackParamList = Record<PageName | 'Home', undefined>;
+
+const { Navigator, Screen } = createNativeStackNavigator<RootStackParamList>();
+
+const pageList = Object.entries(pages).map(
+  ([key, { title, PageComponent }]) => ({
+    pageName: key as PageName,
+    title,
+    PageComponent,
+  })
+);
+
+const HomeScreen = ({
+  navigation,
+}: NativeStackScreenProps<RootStackParamList, 'Home'>) => (
+  <FlatList
+    data={pageList}
+    keyExtractor={(item) => item.pageName}
+    renderItem={({ item: { pageName, title } }) => (
+      <TouchableOpacity onPress={() => navigation.navigate(pageName)}>
+        <Text>{title}</Text>
+      </TouchableOpacity>
+    )}
+  />
+);
+
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Navigator initialRouteName="Home">
+        <Screen name="Home" component={HomeScreen} />
+        {pageList.map(({ pageName, title, PageComponent }) => (
+          <Screen
+            key={pageName}
+            name={pageName}
+            component={PageComponent}
+            options={{ title }}
+          />
+        ))}
+      </Navigator>
+    </NavigationContainer>
   );
-}
+};
+export default App;
 
 const styles = StyleSheet.create({
   container: {
