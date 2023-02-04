457f76a452592965c5f46205ef58a39bcd98fe04
zenatrick <zenatrick@gmail.com>
2023-02-05 03:28:58 +0800

    Add tap gesture detection to TapGestureDemo page

--- pages/TapGestureDemo/index.tsx
+++ pages/TapGestureDemo/index.tsx
@@ -1,10 +1,37 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { ColorValue, StyleSheet, Text, View } from 'react-native';
+import { Gesture, GestureDetector, State } from 'react-native-gesture-handler';
+import Animated, {
+  useAnimatedStyle,
+  useSharedValue,
+} from 'react-native-reanimated';
 
 const title = 'Tap Gesture Demo';
 const TapGestureDemo: React.FC = () => {
+  const backgroundColor = useSharedValue<ColorValue>('transparent');
+  const containerStyle = useAnimatedStyle(() => ({
+    backgroundColor: backgroundColor.value,
+  }));
+  const tapGesture = Gesture.Tap().onFinalize(({ state }) => {
+    switch (state) {
+      case State.END:
+        backgroundColor.value = 'lightgreen';
+        return;
+      case State.FAILED:
+        backgroundColor.value = 'pink';
+        return;
+      default:
+        backgroundColor.value = 'lightgrey';
+        return;
+    }
+  });
+
   return (
     <View style={styles.pageContainer}>
-      <Text>Tap Gesture Demo</Text>
+      <GestureDetector gesture={tapGesture}>
+        <Animated.View style={containerStyle}>
+          <Text>Tap Me!</Text>
+        </Animated.View>
+      </GestureDetector>
     </View>
   );
 };
